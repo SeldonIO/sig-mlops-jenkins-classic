4f049df626d09f6484a7f0d248eee392c637243a
Adrian Gonzalez-Martin <agm@seldon.io>
2020-01-09 10:50:55 +0000

    Split model deployment from cluster setup

--- models/news_classifier/integration/kind_test_all.sh
+++ models/news_classifier/integration/kind_test_all.sh
@@ -9,23 +9,23 @@ service docker start
 # the service can be started but the docker socket not ready, wait for ready
 WAIT_N=0
 while true; do
-    # docker ps -q should only work if the daemon is ready
-    docker ps -q > /dev/null 2>&1 && break
-    if [[ ${WAIT_N} -lt 5 ]]; then
-        WAIT_N=$((WAIT_N+1))
-        echo "[SETUP] Waiting for Docker to be ready, sleeping for ${WAIT_N} seconds ..."
-        sleep ${WAIT_N}
-    else
-        echo "[SETUP] Reached maximum attempts, not waiting any longer ..."
-        break
-    fi
+  # docker ps -q should only work if the daemon is ready
+  docker ps -q >/dev/null 2>&1 && break
+  if [[ ${WAIT_N} -lt 5 ]]; then
+    WAIT_N=$((WAIT_N + 1))
+    echo "[SETUP] Waiting for Docker to be ready, sleeping for ${WAIT_N} seconds ..."
+    sleep ${WAIT_N}
+  else
+    echo "[SETUP] Reached maximum attempts, not waiting any longer ..."
+    break
+  fi
 done
 
 #######################################
 # AVOID EXIT ON ERROR FOR FOLLOWING CMDS
 set +o errexit
 
-# START CLUSTER 
+# START CLUSTER
 make kind_create_cluster
 KIND_EXIT_VALUE=$?
 
@@ -34,17 +34,21 @@ export KUBECONFIG=$(kind get kubeconfig-path)
 
 # ONLY RUN THE FOLLOWING IF SUCCESS
 if [[ ${KIND_EXIT_VALUE} -eq 0 ]]; then
-    # KIND CLUSTER SETUP
-    make kind_setup
-    SETUP_EXIT_VALUE=$?
+  # KIND CLUSTER SETUP
+  make kind_setup
+  SETUP_EXIT_VALUE=$?
 
-    ## INSTALL ALL REQUIRED DEPENDENCIES
-    make install_integration_dev
-    INSTALL_EXIT_VALUE=$?
+  ## INSTALL ALL REQUIRED DEPENDENCIES
+  make install_integration_dev
+  INSTALL_EXIT_VALUE=$?
 
-    ## RUNNING TESTS AND CAPTURING ERROR
-    make test_integration
-    TEST_EXIT_VALUE=$?
+  ## DEPLOY MODEL
+  make deploy_model
+  DEPLOY_MODEL_EXIT_VALUE=$?
+
+  ## RUNNING TESTS AND CAPTURING ERROR
+  make test_integration
+  TEST_EXIT_VALUE=$?
 fi
 
 # DELETE KIND CLUSTER
@@ -61,4 +65,3 @@ service docker stop || true
 
 # NOW THAT WE'VE CLEANED WE CAN EXIT ON TEST EXIT VALUE
 exit ${TEST_EXIT_VALUE}
-
